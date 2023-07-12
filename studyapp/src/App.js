329a9afb84dca0d6be63520428236d67ff90cf91
rushi <hrushikeshmysala2003@gmail.com>
2023-07-12 12:42:35 +0530

    Deleted unecessary files and imported required modules Router set in App.js

--- studyapp/src/App.js
+++ studyapp/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Home from "./components/Home/Home"
 function App() {
-  return (
-    <div>
-      sad
-    </div>
-  );
+  return <Router>
+
+    <Routes>
+
+      <Route path='/' element={<Home />} ></Route>
+    </Routes>
+
+  </Router>
 }
 
 export default App;
--- studyapp/src/index.js
+++ studyapp/src/index.js
@@ -2,7 +2,7 @@ import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
-
+import { ChakraProvider, theme } from '@chakra-ui/react';
 
 
 const container = document.getElementById('root');
@@ -10,8 +10,11 @@ const root = ReactDOM.createRoot(container);
 
 root.render(
   <StrictMode>
+  <ChakraProvider theme={theme}>
     <ColorModeScript />
     <App />
+  </ChakraProvider>
+    
   </StrictMode>
 );
 
